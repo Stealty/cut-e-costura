fa04142c78ff14d47a6c9ea3cd9c853523c5d2e4
Stealty <biellbigama@gmail.com>
2026-02-12 23:32:19 -0300

    fix: remove unsynced speed insights dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -135,7 +134,6 @@ export default function RootLayout({
         <main className="bg-[#f6f1e8] text-[#31382d]  lg:pt-16">{children}</main>
         <Footer />
         <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
